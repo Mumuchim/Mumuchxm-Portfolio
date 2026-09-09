31eb94419c1e54e072a9e15bff5751b2e95133a4
Jerome Isaac Cereneo <mumuchim.jic@gmail.com>
2026-09-10 03:43:59 +0800

    update sep 10.2

--- src/components/TimelineSection.vue
+++ src/components/TimelineSection.vue
@@ -166,7 +166,7 @@ const events = [
     icon: "🟢",
     date: "Apr 2026 — Present",
     title: "FBT Representative",
-    org: "Concentrix|Bytedance · Philippines",
+    org: "Concentrix | Bytedance · Philippines",
     desc: "FBT stands for Fulfilled by TikTok, a Bytedance account. My official title at the Workday level is Customer Service Advisor I. In this role, I act as a CSR (Customer Service Representative) while training the chatbot AI used for TikTok Shop.",
   },
 ];
